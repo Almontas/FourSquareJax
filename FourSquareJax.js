80526c38f490196defcf85567d2f5c55304eb50c
Alex Montas <Alexmontasmusic@gmail.com>
2014-06-26 21:05:52 -0400

    UPDATED JAVASCRIPT

--- FourSquareJax.js
+++ FourSquareJax.js
@@ -1,87 +1,84 @@
-var foursquareApi = {
-            clientId: "4B4T220TQF43NIUKJAQI1DNASEOI12NBTTUNZ5YOETDDQORF",
-            clientSecret: "YDBCTHJECBNMQVFWRC1GU5RFGGPHRY4Y1VGHSVD4PLMGRKNB",
-            redirectUrl : "http://almontas.github.io/FourSquareJax/",
-            
 
-            
+   //gets Authentication Code
+var access_token;        
+    if (window.location.hash) 
+        {
+                                       
+            access_token = window.location.hash.split('=')[1];
+        }
+   $(document).ready(function() {
+    var foursquareApi = {
+                clientId: "4B4T220TQF43NIUKJAQI1DNASEOI12NBTTUNZ5YOETDDQORF",
+                clientSecret: "YDBCTHJECBNMQVFWRC1GU5RFGGPHRY4Y1VGHSVD4PLMGRKNB",
+                redirectUrl : "http://almontas.github.io/FourSquareJax/",
 
-//on CLICK FUNCTION 
-
-            //authorizes my app 
-            authorize: function(){
+    if (access_token.length < 1)
+    {         
+        $('.connect').click (function ()
+            {   
+                  document.getElementById('dashboardProfile').style.display=='none';   
+                  document.getElementById('dashboardInfo').style.display=='none';   
                 var url = "https://foursquare.com/oauth2/access_token";
-                    url += "?client_id="+this.clientId;
+                    url += "?client_id="+foursquareApi.clientId;
                     url += "&response_type=token";
-                    url += "&redirect_uri="+this.redirectUrl;
+                    url += "&redirect_uri="+foursquareApi.redirectUrl;
+                    window.location = url;
+                          
+            });
+    }
+    else 
+    {
+
+    }           
 
 
 
-                    this.getJson(url, function(data){
-                        console.log("authorize",data);
-                    })  //what does this do?
-            },
-            
-            //gets Authentication Code
-            getCode : function(){
-                var url = "http://almontas.github.io/FourSquareJax/";
-                    url += "#access_token=ACCESS_TOKEN";
-    
-                    this.getJson(url, function(data){
-                        console.log("ACCESS_TOKEN",data);
-                    })  //what does this do?
+
+                //gets parameter from above sends request back to API
+
+                //insert function to be sent into API
+                var getCheckin = function () {
+
+                var  userrequest = {userID: self, //stays
+                                    limit: 100,  
+                                    sort: 'newestfirst'  
+                                    };  
+        
+                var result = $.ajax({
+                        url: "https://api.foursquare.com/v2/users/self/checkins?oauth_tokens="+access_token,  
+                        data: userrequest, //stays
+                        dataType: "jsonp",  //stays
+                        type: "GET",  //stays
+                        })
+                .done(function(result){
+                        getData(result);
+                    });
+                })
+                .fail(function(jqXHR, error, errorThrown){ //stays common element
+                    var errorElem = showError(error); //stays common element
+                    $('.search-results').append(errorElem); //stays common element
+                })
 
 
 
-            //gets parameter from above sends request back to API
-            },
-            getJson: function(url, callback){
-                $.getJSON(url, function(data) {
-                  callback(data);
-                });//what does this do?
+                //insert funtion to inject variable into API
+                },
+                getData: function(result){
+                    
+                    var selfcheckin;
+                    $('.checkin').html(result.response.checkins.count);
+                   
 
+                    var countries;
+                    countries.text(result.response.checkins.items.venue.country); // NEED TO LOOP THROUGH THIS
+                    $('.country').append(countries);
 
-            //insert function to be sent into API
-            var getCheckin = function () {
+                    var cities
+                    cities.text(result.response.checkins.items.venue.city); // NEED TO LOOP THROUGH THIS
+                    $('.city').append(countries);
 
-            var  userrequest = {userID: self, //stays
-                                limit: 100,  
-                                sort: 'newestfirst'  
-                                };  
-    
-            var result = $.ajax({
-                    url: "https://api.foursquare.com/v2/users/USER_ID/checkins?oauth_tokens="+access_token,  
-                    data: userrequest, //stays
-                    dataType: "jsonp",  //stays
-                    type: "GET",  //stays
-                    })
-            .done(function(result){
-               
-                });
-            })
-            .fail(function(jqXHR, error, errorThrown){ //stays common element
-                var errorElem = showError(error); //stays common element
-                $('.search-results').append(errorElem); //stays common element
-            })
-
-
-
-            //insert funtion to inject variable into API
-            },
-            getData: function(){
+                }
                 
-                var selfcheckin;
-                selfcheckin.text(result.response.checkins.count);
-                $('.checkin').append(selfcheckin);
-
-                var countries;
-                countries.text(result.response.checkins.items.venue.country); // NEED TO LOOP THROUGH THIS
-                $('.country').append(countries);
-
-                var cities
-                cities.text(result.response.checkins.items.venue.city); // NEED TO LOOP THROUGH THIS
-                $('.city').append(countries);
-
-            }
-            
-        };
+            };
+    };
+  });
