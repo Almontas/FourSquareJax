95a180619ec372af3edc1a11636ae4bea90edb71
Alex Montas <Alexmontasmusic@gmail.com>
2014-06-27 18:26:14 -0400

    Heavily updated JS

--- FourSquareJax.js
+++ FourSquareJax.js
@@ -6,18 +6,25 @@ var access_token;
                                        
             access_token = window.location.hash.split('=')[1];
         }
-   $(document).ready(function() {
+
+$(document).ready(function() {
     var foursquareApi = {
                 clientId: "4B4T220TQF43NIUKJAQI1DNASEOI12NBTTUNZ5YOETDDQORF",
                 clientSecret: "YDBCTHJECBNMQVFWRC1GU5RFGGPHRY4Y1VGHSVD4PLMGRKNB",
-                redirectUrl : "http://almontas.github.io/FourSquareJax/",
+                redirectUrl : "http://almontas.github.io/FourSquareJax/"
+            }
 
-    if (access_token === 0)
-    {         
-        $('.connect').click (function ()
-            {   
-                  document.getElementById('dashboardProfile').style.display=='none';   
-                  document.getElementById('dashboardInfo').style.display=='none';   
+
+    access_token = 3; 
+
+   if (access_token == 3)
+     {       
+        
+        $('.dashboardProfile').hide();   
+        $('.dashboardInfo').hide(); 
+        $('.dashboardConnect').show();   
+        $('.connect').click(function() {   
+                 
                 var url = "https://foursquare.com/oauth2/access_token";
                     url += "?client_id="+foursquareApi.clientId;
                     url += "&response_type=token";
@@ -28,9 +35,9 @@ var access_token;
     }
     else 
     {
-                  document.getElementById('dashboardProfile').style.display=='block';   
-                  document.getElementById('dashboardInfo').style.display=='bloc';  
-    }           
+                  $('.dashboardProfile').show();   
+                  $('.dashboardInfo').show();
+    }             $('.dashboardConnect').hide();
 
 
 
@@ -38,7 +45,8 @@ var access_token;
                 //gets parameter from above sends request back to API
 
                 //insert function to be sent into API
-                var getCheckin = function () {
+                var getCheckin = function () 
+                {
 
                 var  userrequest = {userID: self, //stays
                                     limit: 100,  
@@ -49,22 +57,25 @@ var access_token;
                         url: "https://api.foursquare.com/v2/users/self/checkins?oauth_tokens="+access_token,  
                         data: userrequest, //stays
                         dataType: "jsonp",  //stays
-                        type: "GET",  //stays
+                        type: "GET"  //stays
                         })
                 .done(function(result){
                         getData(result);
-                    });
-                })
+                    })
+                
                 .fail(function(jqXHR, error, errorThrown){ //stays common element
                     var errorElem = showError(error); //stays common element
                     $('.search-results').append(errorElem); //stays common element
-                })
+                });
+                
+            };
 
 
 
-                //insert funtion to inject variable into API
-                },
-                getData: function(result){
+
+
+                var getData = function(result)
+                {
                     
                     var selfcheckin;
                     $('.checkin').html(result.response.checkins.count);
@@ -78,8 +89,8 @@ var access_token;
                     cities.text(result.response.checkins.items.venue.city); // NEED TO LOOP THROUGH THIS
                     $('.city').append(countries);
 
-                }
+                };
                 
-            };
-    };
-  });
+            });
+    
+  
