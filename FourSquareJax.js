0f174f097e6e9b89e730913bc24055abeb5fa040
Alex Montas <Alexmontasmusic@gmail.com>
2014-06-30 11:13:46 -0400

    access_token

--- FourSquareJax.js
+++ FourSquareJax.js
@@ -42,7 +42,7 @@ else {
                 //gets parameter from above sends request back to API
 
                 //insert function to be sent into API
-                var getCheckin = function () 
+                var getCheckin = function (access_token) 
                 {
 
                 var  userrequest = {userID: self, //stays
