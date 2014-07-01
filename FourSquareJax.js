e55f8f749945810d2bdba5f40b186b291e4c0cfe
Alex Montas <Alexmontasmusic@gmail.com>
2014-07-01 18:57:15 -0400

    first and last name

--- FourSquareJax.js
+++ FourSquareJax.js
@@ -126,7 +126,8 @@ var getData = function(result) {
 var getUserData = function(resultinfo) {
     console.log(resultinfo);
     $('.userpic').html('<img src =' + 'https://irs1.4sqi.net/img/user/128x128' + resultinfo.response.user.photo.suffix + '>');
-    $('.username, #fname').html( resultinfo.response.user.firstName );
+    $('.username').html( resultinfo.response.user.firstName + resultinfo.response.user.lastName );
+    $('#fname').html( resultinfo.response.user.firstName);
     $('.homecity').html(resultinfo.response.user.homeCity);
     $('.badges').html(resultinfo.response.user.badges.count + " FourSquare badges");
 };
